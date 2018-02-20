a4f6e057c796ddfc2d36812df9cf64ff6ca2636d
moonrailgun <moonrailgun@gmail.com>
2018-02-20 16:04:52 +0800

    文档速查

---
+++ src/renderer/components/View/Doc.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-row class="doc-container">
+    <el-col :span="9">
+      <el-tree
+      :data="docTree"
+      node-key="id"
+      default-expand-all
+      :expand-on-click-node="false">
+        <span class="custom-tree-node" slot-scope="{ node, data }">
+          <span>{{ node.label }}</span>
+          <span>
+            <el-button
+              type="text"
+              size="mini"
+              @click="() => append(data)">
+              添加
+            </el-button>
+            <el-button
+              type="text"
+              size="mini"
+              v-if="node.id!==0"
+              @click="() => remove(node, data)">
+              移除
+            </el-button>
+          </span>
+        </span>
+      </el-tree>
+    </el-col>
+    <el-col :span="15">
+      2
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  computed: {
+    docTree: {
+      get () {
+        return this.$store.state.View.docTree
+      },
+      set (value) {
+        this.$store.dispatch('modifyDocTree', value)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.doc-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/renderer/datastore.js
+++ src/renderer/datastore.js
@@ -2,6 +2,8 @@ import Datastore from 'nedb'
 import path from 'path'
 import { remote } from 'electron'
 
+console.log('用户文件夹', remote.app.getPath('userData'))
+
 export default new Datastore({
   autoload: true,
   filename: path.join(remote.app.getPath('userData'), '/data.db')
