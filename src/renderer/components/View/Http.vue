84cc591d4d96b4d5e534b0a43a91f0599e12efaa
moonrailgun <moonrailgun@gmail.com>
2018-03-07 11:32:24 +0800

    增加http参数位

--- src/renderer/components/View/Http.vue
+++ src/renderer/components/View/Http.vue
@@ -8,11 +8,23 @@
       <el-input v-model="url" placeholder="请输入URL"></el-input>
       <el-button @click="sendRequest">提交</el-button>
     </div>
+    <div class="params">
+      <div v-for="param in params" class="param">
+        <el-input class="field" v-model="param.field" placeholder="请输入key"></el-input>
+        <span>:</span>
+        <el-input class="value" v-model="param.value" placeholder="请输入value"></el-input>
+      </div>
+      <div class="param">
+        <el-input class="field" v-model="newField" placeholder="请输入key"></el-input>
+        <span>:</span>
+        <el-input class="value" v-model="newValue" placeholder="请输入value"></el-input>
+      </div>
+    </div>
     <div class="body">
       <el-input
         class="response"
         type="textarea"
-        rows="20"
+        :rows="20"
         placeholder="响应结果"
         v-model="response">
       </el-input>
@@ -26,7 +38,10 @@
       return {
         method: 'get',
         url: '',
-        response: ''
+        response: '',
+        params: [],
+        newField: '',
+        newValue: ''
       }
     },
     methods: {
@@ -57,8 +72,27 @@
   margin: 0 2px;
 }
 
+.http .params {
+  margin-top: 10px;
+}
+.http .params .param {
+  flex: 1;
+  display: flex;
+  align-items: center;
+}
+.http .params .field {
+  width: 140px;
+  margin: 0 2px;
+}
+.http .params .value {
+  flex: 1;
+  margin: 0 2px;
+}
+
 .http .body {
   margin-top: 10px;
   padding: 0 2px;
+  padding-top: 10px;
+  border-top: 1px dashed #ccc;
 }
 </style>
