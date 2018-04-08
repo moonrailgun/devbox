c87606e19bb2facf8ed32ae006eca388f4945287
moonrailgun <moonrailgun@gmail.com>
2018-04-08 21:12:31 +0800

    增加端口服务描述

--- src/renderer/components/View/Sys/SysPortScan.vue
+++ src/renderer/components/View/Sys/SysPortScan.vue
@@ -2,12 +2,18 @@
   <div class="sys-portscan">
     <p>端口扫描</p>
     <el-input v-model="host" placeholder="请输入主机" :disabled="isScanning"></el-input>
+    <p class="tip">输入address格式扫描该地址全端口，输入address:port扫描该地址固定端口</p>
     <el-button type="primary" @click="scan()" :loading="isScanning">{{!isScanning?"开始扫描":"扫描中"}}</el-button>
     <p v-if="scanResult.host" class="tip">扫描主机: {{scanResult.host}} 报告</p>
     <el-table :data="scanResult.data" style="width: 100%" v-if="scanResult.host">
       <el-table-column
         prop="port"
         label="端口号"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="服务名"
         width="180">
       </el-table-column>
       <el-table-column
@@ -20,7 +26,7 @@
 
 <script>
   import { ipcRenderer } from 'electron'
-  let portsDesc = {}
+  import portNumbers from 'port-numbers'
 
   export default {
     data () {
@@ -34,22 +40,24 @@
       }
     },
     mounted () {
-      portsDesc = require(__static + '/ports.json')
-
       ipcRenderer.on('port-scan-finished', (event, host, ports) => {
         console.log('扫描完成', host, ports)
         this.$message({
           message: '端口扫描完成',
           type: 'success'
         })
-        this.$set(this.$data, 'isScanning', false)
-        this.$set(this.$data, 'scanResult', {
+        this.isScanning = false
+        this.scanResult = {
           host: host,
-          data: ports.map(p => ({
-            port: p,
-            desc: portsDesc[p] || '-'
-          }))
-        })
+          data: ports.map(p => {
+            let service = portNumbers.getService(p)
+            return {
+              port: p,
+              name: service ? service.name : '-',
+              desc: service ? service.description : '-'
+            }
+          })
+        }
       })
     },
     methods: {
@@ -63,12 +71,18 @@
         }
 
         console.log('开始扫描', this.host)
-        this.$set(this.$data, 'isScanning', true)
-        this.$set(this.$data, 'scanResult', {
+        this.isScanning = true
+        this.scanResult = {
           host: '',
           data: []
-        })
-        ipcRenderer.send('port-scan', this.host)
+        }
+
+        if (this.host.indexOf(':') >= 0) {
+          let [ip, port] = this.host.split(':')
+          ipcRenderer.send('port-scan', ip, Number(port), Number(port))
+        } else {
+          ipcRenderer.send('port-scan', this.host)
+        }
       }
     }
   }
@@ -80,5 +94,6 @@
 }
 .sys-portscan > .tip {
   color: #737373;
+  font-size: 14px;
 }
 </style>
